e481dce311455d923891db7ad328daba1e6139f3
at_fajar <alitaufiqfajar@gmail.com>
2023-03-25 13:49:03 +0700

    tambah assertion untuk event yang ada

--- cypress/integration/akm.ts
+++ cypress/integration/akm.ts
@@ -1,30 +1,11 @@
 import { BiodataPage } from "./pages/biodata_pages"
 const URL = 'https://tryoutakm.bintangpelajar.com'
 
-let listUsernames: string[] = ['PSU004084',
-    'AKM13014195',
-    'PSU004085',
-    'AKM13014196',
-    'AKM13014197',
-    'DJT023001',
-    'DJT023002',
-    'DJT023003',
-    'AKM13014198',
-    'DJT023004',
-    'PSU004086',
-    'PSU004087',
-    'AKM13014199',
-    'PSU004088',
-    'AJB055001',
-    'AJB055002',
-    'PJB054057',
-    'DJK038020',
-    'DSN010043',
-    'DSN010044',
-    'AJB055003',
-    'DJK038021',
-    'AJB055004',
-    'AJB055005'];
+let listUsernames: string[] = [
+"PJB060081"
+];
+
+let nama_event = "Simulasi Kompetisi 2023 | 25 Maret 2023";
 
 describe("Login AKM", function () {
     listUsernames.forEach(testData => {
@@ -34,6 +15,8 @@ describe("Login AKM", function () {
             cy.get("#floatingPassword").type(testData)
             cy.contains("Login").click()
             cy.contains("Success!").should("be.visible")
+            cy.url().should('eq', 'https://tryoutakm.bintangpelajar.com/event')
+            cy.get(".container-fluid").contains(nama_event).should("be.visible")
         });
     });
 })
